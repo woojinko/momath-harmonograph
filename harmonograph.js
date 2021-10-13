bd90dda0fb26497e078f4a1dd72d31a39d7a45ac
woojinko <woojin_ko@berkeley.edu>
2021-10-13 16:11:36 -0400

    improved layout

--- harmonograph.js
+++ harmonograph.js
@@ -19,7 +19,7 @@ var ns, setns = 100000;
 var vis1 = true, vis2 = true;
 
 
-var hScale = 2.5, hWidth = 500, hHeight = 500, hRotation = 0.7854;
+var hScale = 2, hWidth = 500, hHeight = 500, hRotation = 0.7854;
 var dScale = 0.25, dWidth = 180, dHeight = 180, dRotation = hRotation;
 
 
@@ -241,6 +241,37 @@ function savePng() {
 function submitPng() {
 	var res = read('res');
 	save(harmonograph);
+	var options = {
+		files: [
+			// You can specify up to 100 files.
+			{'url': '...', 'filename': '...'},
+			{'url': '...', 'filename': '...'},
+			// ...
+		],
+	
+		// Success is called once all files have been successfully added to the user's
+		// Dropbox, although they may not have synced to the user's devices yet.
+		success: function () {
+			// Indicate to the user that the files have been saved.
+			alert("Success! Files saved to your Dropbox.");
+		},
+	
+		// Progress is called periodically to update the application on the progress
+		// of the user's downloads. The value passed to this callback is a float
+		// between 0 and 1. The progress callback is guaranteed to be called at least
+		// once with the value 1.
+		progress: function (progress) {},
+	
+		// Cancel is called if the user presses the Cancel button or closes the Saver.
+		cancel: function () {},
+	
+		// Error is called in the event of an unexpected response from the server
+		// hosting the files, such as not being able to find a file. This callback is
+		// also called if there is an error on Dropbox or if the user is over quota.
+		error: function (errorMessage) {}
+	};
+	var dropboxButton = Dropbox.createSaveButton(options);
+	document.getElementById("draw").appendChild(dropboxButton);
 }
 
 // try to implement later if I have time!
