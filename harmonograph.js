a885fb336f26d4232609ebfec80f62d14d27092d
woojinko <woojin_ko@berkeley.edu>
2021-10-20 14:23:13 -0400

    tried moving pendulum diagram to pendinit, failed, reverted with comments for now

--- harmonograph.js
+++ harmonograph.js
@@ -30,6 +30,7 @@ var dScale = 0.25, dX = 110, dY = 180, dRotation = hRotation;
 function init() {
 	harmonInit();
 	// diagramInit();
+	// pendInit();
 	t = 0.0; ns = setns;
 	inputChange();
 	swing();
@@ -46,7 +47,7 @@ function harmonInit() {
 
 	// erase existing pixels in drawing area by setting them to transparent black
 	hg.clearRect(-1 * hX, -1 * hY, 880, 880);
-	
+
 	hg.rotate(hRotation);
 
 	// set up aesthetics
@@ -58,6 +59,45 @@ function harmonInit() {
 	hg.lineWidth = 0.5;
 	hg.globalAlpha = 0.75;
 }
+
+// function pendInit() {
+// 	var canvas = document.getElementById('pendCanvas');
+// 	var context = canvas.getContext('2d');
+// 	// context.clearRect(0, 0, canvas.width, canvas.height);
+// 	// context.beginPath();
+// 	var prev = 0;
+// 	var sim = PendulumSim(1, 9.80665, Math.PI * 15 / 100, 10, function (angle) {
+// 		var rPend = Math.min(canvas.width, canvas.height) * 0.47;
+// 		var rBall = Math.min(canvas.width, canvas.height) * 0.02;
+// 		var rBar = Math.min(canvas.width, canvas.height) * 0.005;
+// 		var ballX = Math.sin(angle) * rPend;
+// 		var ballY = Math.cos(angle) * rPend;
+
+// 		context.fillStyle = "rgba(255,255,255,0.51)";
+// 		context.globalCompositeOperation = "destination-out";
+// 		context.fillRect(0, 0, canvas.width, canvas.height);
+
+// 		context.fillStyle = "yellow";
+// 		context.strokeStyle = "rgba(0,0,0," + Math.max(0, 1 - Math.abs(prev - angle) * 10) + ")";
+// 		context.globalCompositeOperation = "source-over";
+
+// 		context.save();
+// 		context.translate(canvas.width / 2, canvas.height / 2);
+// 		context.rotate(angle);
+
+// 		context.beginPath();
+// 		context.rect(-rBar, -rBar, rBar * 2, rPend + rBar * 2);
+// 		context.fill();
+// 		context.stroke();
+
+// 		context.beginPath();
+// 		context.arc(0, rPend, rBall, 0, Math.PI * 2, false);
+// 		context.fill();
+// 		context.stroke();
+// 		context.restore();
+// 		prev = angle;
+// 	});
+// }
 
 // // helper function to initialize diagram
 // function diagramInit() {
@@ -148,13 +188,13 @@ function step() {
 			// 	// 	penColor = style.getPropertyValue("--pen-color-5");
 			// 	// }
 			// } else {
-				// if (t % 3 < 1) {
-				// 	penColor = style.getPropertyValue("--pen-color-3");
-				// } else if (t % 3 < 2) {
-				// 	penColor = style.getPropertyValue("--pen-color-4");
-				// } else {
-				// 	penColor = style.getPropertyValue("--pen-color-5");
-				// }
+			// if (t % 3 < 1) {
+			// 	penColor = style.getPropertyValue("--pen-color-3");
+			// } else if (t % 3 < 2) {
+			// 	penColor = style.getPropertyValue("--pen-color-4");
+			// } else {
+			// 	penColor = style.getPropertyValue("--pen-color-5");
+			// }
 			// }
 			penColor = style.getPropertyValue("--current-pen-color");
 			hg.strokeStyle = penColor;
@@ -314,3 +354,15 @@ function resetParams() {
 	document.getElementById("resetParam").reset();
 }
 
+// function PendulumSim(length_m, gravity_mps2, initialAngle_rad, timestep_ms, callback) {
+// 	var velocity = 0;
+// 	var angle = initialAngle_rad;
+// 	var k = -gravity_mps2 / length_m;
+// 	var timestep_s = timestep_ms / 1000;
+// 	return setInterval(function () {
+// 		var acceleration = k * Math.sin(angle);
+// 		velocity += acceleration * timestep_s;
+// 		angle += velocity * timestep_s;
+// 		callback(angle);
+// 	}, timestep_ms);
+// }
